9ff89f63f8134114f0ac815e59454e35f67064a7
GraHub <38760373+GraHub@users.noreply.github.com>
2020-09-09 01:06:43 -0300

    app files

---
+++ spark-digital/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SquaresComponent } from './squares/squares.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquaresComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
